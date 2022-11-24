c6cc022837a69e8f695e24f902349ac2e5806273
RomanTkachenko1996 <110996538+RomanTkachenko1996@users.noreply.github.com>
2022-11-24 10:34:28 +0200

    edited MyArrayList

--- src/task_9/MyArrayList.java
+++ src/task_9/MyArrayList.java
@@ -1,15 +1,56 @@
 package task_9;
 
+import java.util.Arrays;
+
 public class MyArrayList {
-    public void add(Object value) {
+    private int [] data = new int[8];
+    int index = 0;
+    public void add(int value) {
+        increaseSize();
+        data[index] = value;
+        index++;
     }
+
+    private void increaseSize() {
+        if (index==data.length){
+            int [] copy = new int[data.length*2];
+            System.arraycopy(data,0,copy,0,data.length);
+            data = copy;
+        }
+    }
+
     public void remove(int index) {
     }
     public void clear() {
+        data = new int[8];
+        index = 0;
     }
-    public void size() {
+    public int size() {
+        return index;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        return (builder.append(Arrays.toString(data))).toString();
+    }
+
     public int get(int index) {
-        return 0;
+        return data[index];
+    }
+
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        for (int i = 0; i <10; i++) {
+            list.add(10);
+        }
+        System.out.println("list = " + list);
+        System.out.println("list.get(0) = " + list.get(0));
+        System.out.println("list.size() = " + list.size());
+        list.clear();
+        System.out.println("list = " + list);
+        System.out.println("list.size() = " + list.size());
+
+
     }
 }
