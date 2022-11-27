608e742e6433f1f5eda6f8272aa39003315e3914
RomanTkachenko1996 <110996538+RomanTkachenko1996@users.noreply.github.com>
2022-11-27 10:03:39 +0200

    edited MyLinkedList

--- src/task_9/MyLinkedList.java
+++ src/task_9/MyLinkedList.java
@@ -2,69 +2,82 @@ package task_9;
 
 import java.util.Arrays;
 
-public class MyLinkedList<T> {
-    static int size = 0;
-    Node<T> head;
-    Node<T> tale;
-    Node<T> next;
+public class MyLinkedList<T> { private static int size = 0;
+   private Node<T> first;
+    private Node<T> last;
 
     private static class Node<T> {
-        T item;
+        T element;
         Node<T> next;
-        Node<T> prev;
-
-        Node(Node<T> prev, T element, Node<T> next) {
-            this.item = element;
-            this.next = next;
-            this.prev = prev;
+        Node( T element) {
+            this.element = element;
         }
     }
 
-    public void add(T value) {
-        Node<T> previousNode = tale;
-        Node<T> newNode = new Node<>(previousNode, value, null);
-        tale = newNode;
-        if (previousNode == null) {
-            head = newNode;
+    public void add(T element) {
+        Node <T> newNode = new Node<>(element);
+        if (first == null){
+            first = last = newNode;
         } else {
-            previousNode.next = newNode;
+            last.next = newNode;
+            last = newNode;
+        }
+        size++;
+    }
+    public void add(T element, int index) {
+        checkIndex(index);
+        Node <T> newNode = new Node<>(element);
+        if (first == null){
+            first = last = newNode;
+        } else if (index == 0) {
+            last.next = newNode;
+            last = newNode;
+        } else {
+            Node<T> current = getNodeByIndex(index-1);
+            newNode.next = current.next;
+            current.next = newNode;
         }
         size++;
     }
 
-    public T remove(int index) {
-        Node<T> x = head;
-        for (int i = 0; i < index; i++) {
-            x = x.next;
+    private void checkIndex(int index) {
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException();
         }
-        T element = x.item;
-        Node<T> next = x.next;
-        Node<T> prev = x.prev;
-        if (prev == null) {
-            head = next;
-        } else {
-            prev.next = next;
-            x.prev = null;
-        }
-
-        if (next == null) {
-            tale = prev;
-        } else {
-            next.prev = prev;
-            x.next = null;
-        }
-
-        x.item = null;
-        size--;
-        return element;
     }
 
-    public boolean clear() {
-        next = null;
-        head = null;
-        tale = null;
+    private Node<T> getNodeByIndex(int index) {
+        Node <T> current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public T remove(int index) {
+        checkIndex(index);
+        T elementToRemove;
+        if (index == 0){
+            elementToRemove = first.element;
+            first = first.next;
+            if (first == null){
+               last = null;
+            }
+        } else{
+            Node<T> prev = getNodeByIndex(index - 1);
+            elementToRemove = prev.next.element;
+            prev.next = prev.next.next;
+            if (index == size-1){
+                last = prev;
+            }
+        }
+        size--;
+        return elementToRemove;
+    }
+
+    public void clear() {
+        first = last = null;
         size = 0;
-        return true;
     }
 
     public int size() {
@@ -72,19 +85,16 @@ public class MyLinkedList<T> {
     }
 
     public T get(int index) {
-        Node<T> x = head;
-        for (int i = 0; i < index; i++) {
-            x = x.next;
-        }
-        return x.item;
+        checkIndex(index);
+        return getNodeByIndex(index).element;
     }
 
     @Override
     public String toString() {
         Object[] result = new Object[size];
         int i = 0;
-        for (Node<T> x = head; x != null; x = x.next) {
-            result[i++] = x.item;
+        for (Node<T> x = first; x != null; x = x.next) {
+            result[i++] = x.element;
         }
         return Arrays.toString(result);
     }
